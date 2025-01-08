fb4fcf164475737d21ef28c46209c3c5be68fc85
cemresvural <cemresvural@gmail.com>
2025-01-08 23:22:43 +0300

    radio button is checked for genders

--- src/test/java/TestGenderSelection.java
+++ src/test/java/TestGenderSelection.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestGenderSelection extends BaseTest{
@@ -6,16 +7,22 @@ public class TestGenderSelection extends BaseTest{
     @Test
     public void checkMaleRadioButton(){
         practiceFromPage.genderSection().clickRadioButton(GenderSection.Genders.MALE);
+        Assertions.assertTrue(practiceFromPage.genderSection().isRadioButtonChecked(GenderSection.Genders.MALE),
+                "Male radio button is not checked!");
 
     }
     @Test
     public void checkFemaleRadioButton(){
         practiceFromPage.genderSection().clickRadioButton(GenderSection.Genders.FEMALE);
+        Assertions.assertTrue(practiceFromPage.genderSection().isRadioButtonChecked(GenderSection.Genders.FEMALE),
+                "Female radio button is not checked!");
 
     }
     @Test
     public void checkOtherRadioButton(){
         practiceFromPage.genderSection().clickRadioButton(GenderSection.Genders.OTHER);
+        Assertions.assertTrue(practiceFromPage.genderSection().isRadioButtonChecked(GenderSection.Genders.OTHER),
+                "Other radio button is not checked!");
 
     }
 
