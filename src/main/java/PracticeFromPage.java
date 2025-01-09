b8e7aab98b2ffc7db4cb8469b5563f83a2770358
cemresvural <cemresvural@gmail.com>
2025-01-09 21:37:05 +0300

    refactor

--- src/main/java/PracticeFromPage.java
+++ src/main/java/PracticeFromPage.java
@@ -2,52 +2,40 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class PracticeFromPage {
-    private WebDriver driver;
-    private final By name=By.id("firstName");
-    private final By lastName=By.id("lastName");
-    private final By email=By.id("userEmail");
+public class PracticeFromPage extends BasePage{
+
+    private final By nameLocater =By.id("firstName");
+    private final By lastNameLocater =By.id("lastName");
+    private final By emailLocater =By.id("userEmail");
 
     private GenderSection genderSection;
 
     public PracticeFromPage(WebDriver driver){
-        this.driver=driver;
+        super(driver);
         genderSection=new GenderSection(driver);
     }
     public GenderSection genderSection(){
         return this.genderSection;
     }
 
-    public void setName(String nameAsString){
-        WebElement nameSpace=driver.findElement(name);
-        nameSpace.click();
-        nameSpace.sendKeys(nameAsString);
+    public void setName(String name){
+      type(nameLocater,name);
 
     }
-    public void setLastName(String setLastNameAsString){
-        WebElement nameSpace=driver.findElement(lastName);
-        nameSpace.click();
-        nameSpace.sendKeys(setLastNameAsString);
+    public void setLastName(String lastName){
+        type(lastNameLocater,lastName);
 
     }
-    public void setEmail(String emailAsString){
-        WebElement nameSpace=driver.findElement(email);
-        nameSpace.click();
-        nameSpace.sendKeys(emailAsString);
-
+    public void setEmail(String email){
+      type(emailLocater,email);
     }
     public String getName(){
-        //inspect elementi incelediğimde text yok bu yüzden get text methodu ile alamıyorum
-        WebElement nameSpace= driver.findElement(name);
-        return nameSpace.getAttribute("value");
-        //burada kullanacağımız attribute parametre olarak value olacak çünkü text vesaire yok, olmadığında bu şekilde alabiliyorum.
+        return find(nameLocater).getAttribute("value");
     }
     public String getLastname(){
-        WebElement lastnameSpace=driver.findElement(lastName);
-        return lastnameSpace.getAttribute("value");
+        return find(lastNameLocater).getAttribute("value");
     }
     public String getEmail(){
-        WebElement emailSpace=driver.findElement(email);
-        return emailSpace.getAttribute("value");
+        return find(emailLocater).getAttribute("value");
     }
 }
